3627ffe258115d3a2661e964b2e2838e425c0745
m12watanabe1a <40206149+m12watanabe1a@users.noreply.github.com>
2022-12-14 12:14:51 +0900

    add watchdog and fix format

--- p9n_node/src/teleop_twist_joy_node.cpp
+++ p9n_node/src/teleop_twist_joy_node.cpp
@@ -17,85 +17,79 @@
 
 namespace p9n_node
 {
-
 TeleopTwistJoyNode::TeleopTwistJoyNode(const rclcpp::NodeOptions & options)
 : rclcpp::Node("teleop_twist_joy_node", options)
 {
-
   const std::string hw_name = this->declare_parameter<std::string>(
     "hw_type", p9n_interface::HW_NAME::DUALSENSE);
+  this->linear_max_speed_ =
+    this->declare_parameter<double>("linear_speed", 0.2);
+  this->angular_max_speed_ =
+    this->declare_parameter<double>("angular_speed", 0.6);
 
   try {
     this->hw_type_ = p9n_interface::getHwType(hw_name);
   } catch (std::runtime_error & e) {
+    RCLCPP_ERROR(this->get_logger(), e.what());
     RCLCPP_ERROR(
-      this->get_logger(),
-      e.what());
-    RCLCPP_ERROR(
-      this->get_logger(),
-      "Please select hardware from %s",
+      this->get_logger(), "Please select hardware from %s",
       p9n_interface::getAllHwName().c_str());
-    rclcpp::shutdown();
+    exit(EXIT_FAILURE);
     return;
   }
 
   this->p9n_if_ =
     std::make_unique<p9n_interface::PlayStationInterface>(this->hw_type_);
 
-  this->joy_sub_ = this->create_subscription<sensor_msgs::msg::Joy>(
-    "joy", rclcpp::SensorDataQoS(rclcpp::KeepLast(1)),
-    std::bind(&TeleopTwistJoyNode::onJoy, this, std::placeholders::_1));
+  using namespace std::placeholders;  // NOLINT
+  this->joy_sub_ = this->create_subscription<Joy>(
+    "joy", rclcpp::SensorDataQoS().keep_last(1),
+    std::bind(&TeleopTwistJoyNode::onJoy, this, _1));
 
-  this->twist_pub_ = this->create_publisher<geometry_msgs::msg::Twist>(
-    "cmd_vel", rclcpp::QoS(10));
+  this->twist_pub_ = this->create_publisher<Twist>(
+    "cmd_vel", rclcpp::QoS(10).reliable().durability_volatile());
 
+
+  using namespace std::chrono_literals; // NOLINT
+  this->timer_watchdog_ = this->create_wall_timer(
+    1s, std::bind(&TeleopTwistJoyNode::onWatchdog, this));
 
   if (this->joy_sub_->get_publisher_count() == 0) {
-    RCLCPP_WARN(
-      this->get_logger(),
-      "Joy node not launched");
+    RCLCPP_WARN(this->get_logger(), "Joy node not launched");
   }
 }
 
-void TeleopTwistJoyNode::onJoy(sensor_msgs::msg::Joy::ConstSharedPtr joy_msg)
+void TeleopTwistJoyNode::onJoy(Joy::ConstSharedPtr joy_msg)
 {
+  this->timer_watchdog_->reset();
   this->p9n_if_->setJoyMsg(joy_msg);
 
-  if (this->p9n_if_->pressedR1()) {
-    speed_factor_ = speed_factor_ * 1.1;
-    RCLCPP_INFO(
-      this->get_logger(),
-      "Max speed: %.3f", speed_factor_);
+  static bool stopped = true;
+  if (this->p9n_if_->isTiltedStickL()) {
+    auto twist_msg = std::make_unique<Twist>();
+    twist_msg->linear.set__x(this->linear_max_speed_ * this->p9n_if_->tiltedStickLY());
+    twist_msg->angular.set__z(this->angular_max_speed_ * this->p9n_if_->tiltedStickLX());
+    this->twist_pub_->publish(std::move(twist_msg));
+
+    stopped = false;
   }
-  if (this->p9n_if_->pressedL1()) {
-    speed_factor_ = speed_factor_ * 0.9;
-    RCLCPP_INFO(
-      this->get_logger(),
-      "Max speed: %.3f", speed_factor_);
+  if (!stopped) {
+    // Stop cart
+    auto twist_msg = std::make_unique<Twist>(rosidl_runtime_cpp::MessageInitialization::ZERO);
+    this->twist_pub_->publish(std::move(twist_msg));
+
+    stopped = true;
   }
+}
 
-  auto twist = geometry_msgs::msg::Twist();
-  if (
-    std::abs(this->p9n_if_->tiltedStickLX()) > 1e-2 ||
-    std::abs(this->p9n_if_->tiltedStickLY()) > 1e-2)
-  {
-    float l_y = this->p9n_if_->tiltedStickLY();
-    float l_x = this->p9n_if_->tiltedStickLX();
+void TeleopTwistJoyNode::onWatchdog()
+{
+  RCLCPP_WARN(this->get_logger(), "Couldn't subscribe joy topic before timeout");
 
-    twist.linear.x = speed_factor_ * l_y;
-    twist.angular.z = M_PI * speed_factor_ * l_x;
+  // Publish zero velocity to stop vehicle
+  auto twist_msg = std::make_unique<Twist>(rosidl_runtime_cpp::MessageInitialization::ZERO);
+  this->twist_pub_->publish(std::move(twist_msg));
 
-    this->twist_pub_->publish(twist);
-
-    // never wait for chattering for this scheme
-    return;
-  } else {
-    twist.linear.x = 0.0;
-    twist.angular.z = 0.0;
-    this->twist_pub_->publish(twist);
-  }
-
-  using namespace std::chrono_literals;
-  rclcpp::sleep_for(200ms);
+  this->timer_watchdog_->cancel();
 }
 }  // namespace p9n_node
